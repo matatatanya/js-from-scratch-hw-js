a51a75abca5791b3cbf7b4ce3b9931c968ec359e
matatatanya <t.matusevich.v@gmail.com>
2026-05-24 21:36:41 +0300

    lesson-2-1

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -8,11 +8,19 @@
 */
 
 // тестовые данные (значения можно менять)
-const isAdmin = false
-const isVerifiedUser = true
-const hasSpecialPermission = true
-const hasTemporaryPass = false
+const isAdmin = false;
+const isVerifiedUser = true;
+const hasSpecialPermission = true;
+const hasTemporaryPass = false;
 
-let isAccess
+let isAccess = false;
 
 // your code
+// if ((isAdmin === true || isVerifiedUser === true) && (hasSpecialPermission === true || hasTemporaryPass === true)) {
+//     isAccess = true;
+// }
+
+
+if ((isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass)) {
+    isAccess = true;
+}
