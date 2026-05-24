4ad9227e165a44bbd8df7ecdf4e4ba1ce5d2dc9a
matatatanya <t.matusevich.v@gmail.com>
2026-05-24 23:10:13 +0300

    lesson-2-2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,16 +7,19 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.floor(Math.random() * 6 + 1);
+let dice2 = Math.floor(Math.random() * 6 + 1);
+let isWinningDouble = false;
 
-/*
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
-} else {
-  console.log('Не выигрышный дубль.')
+if (dice1 === dice2 && dice2 > 3){
+  isWinningDouble = true;
 }
-*/
+
+console.log('Первый бросок: ' + dice1);
+console.log('Второй бросок: ' + dice2);
+if (isWinningDouble) {
+  console.log('Выигрышный дубль!');
+} else {
+  console.log('Не выигрышный дубль.');
+}
+
